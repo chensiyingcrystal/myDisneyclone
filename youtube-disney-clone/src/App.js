7b4d0ef5c068fe0300990c4cf70089df52984172
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-23 23:25:03 -0800

    react-router-dom; create Login

--- youtube-disney-clone/src/App.js
+++ youtube-disney-clone/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello YouTube and Crystal! Let's build the Disney+ Clone!</h2>
+    <div className="App"> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
